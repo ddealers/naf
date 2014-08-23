e0eaf327e1e5e8de5b004ec6e7f90bbc4516eb2e
Wenceslao Negrete <vampediem@gmail.com>
2014-08-23 04:14:02 -0500

    -___________- zzzz

--- _nivel4/js/main.js
+++ _nivel4/js/main.js
@@ -1,20 +1,49 @@
 (function() {
-  var evaluate, load;
+  var evaluate, evaluateList, load;
 
   load = function(url) {
     var cont;
     cont = $('#cont');
     cont.empty();
     return cont.load(url, function() {
-      return $('.eval').on('click', function(e) {
+      $('.list input[type=checkbox]').val('false');
+      $('.list input[type=checkbox]').on('change', function() {
+        var cb;
+        cb = $(this);
+        return cb.val(cb.prop('checked'));
+      });
+      return $('button.eval').on('click', function(e) {
         var draw, form;
         e.preventDefault();
         form = $(e.target).data('target');
         draw = $(e.target).data('draw');
         console.log(form, draw);
-        return evaluate(form, draw);
+        if ($('#' + form).hasClass('list')) {
+          return evaluateList(form, draw);
+        } else {
+          return evaluate(form, draw);
+        }
       });
     });
+  };
+
+  evaluateList = function(form, draw) {
+    var checked, item, list, response, _ref;
+    draw = '.alert-box > div';
+    response = '<p>1. Correct</p>';
+    list = v[$('#' + form).data('list')];
+    for (item in list) {
+      checked = (_ref = $('[name="' + item + '"]').prop('checked')) != null ? _ref : {
+        on: false
+      };
+      if (!checked === list[item]) {
+        response = '<p>1. Incorrect</p>';
+      }
+    }
+    $(draw).empty();
+    $(draw).append('<a href="#" class="close">x</a>');
+    $(draw).append(response);
+    return $('.alert-box').fadeIn(500);
   };
 
   evaluate = function(form, draw) {
@@ -25,14 +54,24 @@
     $(draw).empty();
     $(draw).append('<a href="#" class="close">x</a>');
     return $.each(answers, function(index, elem) {
-      var answer, correct;
+      var answer, correct, key, _i, _len;
       answer = elem.value;
       correct = v[elem.name];
-      console.log(answer, correct, String(answer) === String(correct));
-      if (String(answer).toLowerCase() === String(correct).toLowerCase()) {
-        $(draw).append('<p>' + (index + 1) + '. Correct</p>');
+      if (Object.prototype.toString.call(correct) === '[object Array]') {
+        for (_i = 0, _len = correct.length; _i < _len; _i++) {
+          key = correct[_i];
+          if (String(answer).toLowerCase() === String(correct[key]).toLowerCase()) {
+            $(draw).append('<p>' + (index + 1) + '. Correct</p>');
+          } else {
+            $(draw).append('<p>' + (index + 1) + '. Incorrect</p>');
+          }
+        }
       } else {
-        $(draw).append('<p>' + (index + 1) + '. Incorrect</p>');
+        if (String(answer).toLowerCase() === String(correct).toLowerCase()) {
+          $(draw).append('<p>' + (index + 1) + '. Correct</p>');
+        } else {
+          $(draw).append('<p>' + (index + 1) + '. Incorrect</p>');
+        }
       }
       return $('.alert-box').fadeIn(500);
     });
